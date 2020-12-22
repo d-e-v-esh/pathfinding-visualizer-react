c99a4e2abe61fce4e13664ad5ef551951d284716
d-e-v-esh <59534570+d-e-v-esh@users.noreply.github.com>
2020-12-22 21:19:22 +0530

    💡 Scrapped Everything (New Idea)

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,65 +1,7 @@
 import React, { useState, useEffect } from "react";
-import Node from "../components/Node";
-
-let START_NODE_ROW = 12;
-let START_NODE_COL = 20;
-let END_NODE_ROW = 12;
-let END_NODE_COL = 40;
 
 const Board = () => {
-  const [grid, setGrid] = useState([]);
-  const [isMousePressed] = useState(false);
-
-  const createNode = (row, col) => ({
-    row,
-    col,
-    isWall: false,
-    isStart: row === START_NODE_ROW && col === START_NODE_COL,
-    isEnd: row === END_NODE_ROW && col === END_NODE_COL,
-    distance: Infinity,
-    isVisited: false,
-    previousNode: null,
-    f: 0,
-    g: 0,
-    h: 0,
-  });
-  const ROW = 30;
-  const COL = 65;
-  const createInitialGrid = () => {
-    const grid = [];
-    for (let row = 0; row < ROW; row++) {
-      const rowArray = [];
-      for (let col = 0; col < COL; col++) {
-        rowArray.push(createNode(row, col));
-      }
-      grid.push(rowArray);
-    }
-    return grid;
-  };
-
-  useEffect(() => {
-    const grid = createInitialGrid();
-    setGrid({ grid });
-  });
-  return (
-    <div>
-      <div className="visualizer-container">
-        {grid.map((rowArray, rowIdx) => (
-          <div key={rowIdx}>
-            {rowArray.map((node, nodeIdx) => (
-              <Node
-                key={nodeIdx}
-                row={node.row}
-                col={node.col}
-                isStart={node.isStart}
-                isEnd={node.isEnd}
-              />
-            ))}
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Board;
--- src/components/Node.jsx
+++ src/components/Node.jsx
@@ -1,29 +1,9 @@
 import React from "react";
 
-import "./Node.css";
+// import "./Node.css";
 
-const Node = (props) => {
-  const { row, col, isWall, isStart, isEnd } = props;
-
-  const node_status = isStart
-    ? "start_node"
-    : isEnd
-    ? "end_node"
-    : isWall
-    ? "wall_node"
-    : "";
-
-  return (
-    <div
-      id={`node-${row}-${col}`}
-      className={`${props.className} node ${node_status}`}
-      onMouseDown={(e) => {
-        props.onMouseDown(row, col);
-        e.preventDefault();
-      }}
-      onMouseEnter={() => props.onMouseEnter(row, col)}
-      onMouseUp={props.onMouseUp}></div>
-  );
+const Node = () => {
+  return <div className="node"></div>;
 };
 
 export default Node;
